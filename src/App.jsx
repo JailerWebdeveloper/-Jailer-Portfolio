60ae2cc0473d9036526471af2af5f74f987dbd21
Jailerwebwork <jailerwebwork@gmail.com>
2023-12-16 14:10:14 -0500

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import html from "./assets/html.png";
 import js from "./assets/js.png";
 import react from "./assets/react.png";
 import figmaland from "./assets/figmaland.png";
+import Monitor from "./assets/Monitor.png";
 import tailwind from "./assets/tailwind.png";
 import github from "./assets/github.png";
 import database from "./assets/database.png";
@@ -26,9 +27,6 @@ import python from "./assets/Python.png";
 import reactc from "./assets/react.jpg";
 import git from "./assets/git.png";
 function App() {
-
-  
-
   const settings = {
     dots: true,
     infinite: true,
@@ -50,7 +48,6 @@ function App() {
           slidesToScroll: 1,
         },
       },
-      
     ],
   };
 
@@ -123,20 +120,26 @@ function App() {
       id: 1,
       name: "Alura-Encriptador",
       description: "Responsive Text Encryptor and Decryptor",
-      link:"https://jailerwebdeveloper.github.io/Encriptador-alura/",
-      image:Encriptador,
+      link: "https://jailerwebdeveloper.github.io/Encriptador-alura/",
+      image: Encriptador,
       tags: "HTML, CSS, JS",
     },
     {
       id: 2,
       name: "Landing-page",
       description: "Landing-page made with Boostrap and Html5 ",
-      link:"https://jailerwebdeveloper.github.io/LandingPage-With-Boostrap/",
-      image:figmaland,
+      link: "https://jailerwebdeveloper.github.io/LandingPage-With-Boostrap/",
+      image: figmaland,
       tags: "HTML, CSS, Boostrap5",
     },
-
-
+    {
+      id: 4,
+      name: "Financial monitor",
+      description: "Application for monitoring the states of currencies and stocks using the World Bank API.",
+      link: "https://monitorfinancieroupc.netlify.app",
+      image: Monitor,
+      tags: "HTML, CSS, Tailwind, Reactjs",
+    },
   ]);
   return (
     <>
@@ -147,13 +150,16 @@ function App() {
           <div className="hero-overlay bg-opacity-60 "></div>
           <div className="hero-content text-center text-neutral-content ">
             <div className="max-w-md">
-              <p className="text-6xl bebas bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent  ">I'M JAILER </p>
+              <p className="text-6xl bebas bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent  ">
+                I'M JAILER{" "}
+              </p>
               <div className="avatar my-7  group">
                 <div className="w-24 mask mask-hexagon relative">
-                  <img src={logo1}  className=""/>
-                 
+                  <img src={logo1} className="" />
                 </div>
-                <a className="rounded-full absolute -bottom-5 left-7 w-10 h-10 bg-[#1d232a] p-2  "><PiHandWavingDuotone className="w-full h-full text-yellow-500 wave"/></a>
+                <a className="rounded-full absolute -bottom-5 left-7 w-10 h-10 bg-[#1d232a] p-2  ">
+                  <PiHandWavingDuotone className="w-full h-full text-yellow-500 wave" />
+                </a>
               </div>
               <h1 className="my-5 text-7xl font-bold bebas  bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
                 FRONT END <br /> DEVELOPER
@@ -172,15 +178,13 @@ function App() {
         </Element>
       </div>
       {/*Hero*/}
-      <Element name="aboutme" className="flex justify-center w-full h-screen">
+      <Element name="aboutme" className="flex justify-center w-full h-60 my-5 mt-20">
         <div className="hero">
-          <div className="hero-content w-full flex-col lg:flex-row-reverse">
-            <img
-              src={foto}
-              className="max-w-xs rounded-full shadow-2xl p-6"
-            />
+          <div className=" w-full flex-col lg:flex-row-reverse">
             <div>
-              <h1 className="text-5xl font-bold text-shadow text-white sm:text-center">HI! I'M JAILER</h1>
+              <h1 className="text-5xl font-bold text-shadow text-white sm:text-center">
+                HI! I'M JAILER
+              </h1>
               <p className="py-6 max-w-2xl break-words text-white sm:text-justify sm:w-4/5 sm:mx-auto ">
                 I am a young student of software engineering with extensive
                 knowledge in everything related to front-end development applied
@@ -195,12 +199,9 @@ function App() {
           </div>
         </div>
       </Element>
-      <Element
-        name="proyectos"
-        className="p-10 flex justify-center flex-col"
-      >
+      <Element name="proyectos" className="p-10 flex justify-center flex-col">
         <span className="text-6xl text-white text-center bebas text-shadow bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-        Projects
+          Projects
         </span>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-auto h-full gap-4 p-10 place-items-center  my-5">
           {projects.map((project) => (
@@ -217,7 +218,8 @@ function App() {
           Skills
         </span>
         <span className=" text-center text-xl text-white break-words	">
-        Since 2020 studying on my own and numerous courses to always be prepared
+          Since 2020 studying on my own and numerous courses to always be
+          prepared
         </span>
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 my-5 w-full gap-8 md:w-1/2  md:mx-auto">
           {skills.map((skill) => (
@@ -233,14 +235,14 @@ function App() {
 
       <Element name="Formacion" className="flex flex-col justify-center w-full">
         <span className="text-6xl text-center text-white bebas mt-10 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-shadow">
-        Certificates
+          Certificates
         </span>
         <div className=" flex justify-center">
-        <Slider {...settings} className="w-4/5 h-full  my-2">
-          {courses.map((course) => (
+          <Slider {...settings} className="w-4/5 h-full  my-2">
+            {courses.map((course) => (
               <Course key={course.id} course={course} />
-          ))}
-        </Slider>
+            ))}
+          </Slider>
         </div>
       </Element>
     </>
